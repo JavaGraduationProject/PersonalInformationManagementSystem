f0bf19d7eed4c233097416936caaa2b5c83c8a81
Ganson <1145317236@qq.com>
2024-09-10 02:09:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# 全目录
+
+[更多系统、论文，供君选择 ~~>](https://www.bitwise.net.cn)
+
+# 204.PersonalInformationManagementSystem
+
+<p>群: 983063232(入群获取sql文件)</p>
+<p>QQ: 206157502(加好友获取sql文件)</p>
+
+<p><h1 align="center">204.个人信息管理系统</h1></p>
+
+
+<p align="center">
+	<img src="https://img.shields.io/badge/jdk-1.8-orange.svg"/>
+    <img src="https://img.shields.io/badge/servlet-5.x-lightgrey.svg"/>
+    <img src="https://img.shields.io/badge/jsp-3.x-blue.svg"/>
+    <img src="https://img.shields.io/badge/jdbc-5.x-yellow.svg"/>
+</p>
+
+# 简介
+
+
+> 本代码来源于网络,仅供学习参考使用,请入群(983063232)后联系群主索要sql文件!
+>
+> 提供1.远程部署/2.修改代码/3.设计文档指导/4.框架代码讲解等服务
+> 
+> http://localhost:8084/PIMS/login.jsp
+> 
+> admin1   admin1
+
+# 环境
+
+- <b>IntelliJ IDEA 2009.3</b>
+
+- <b>Mysql 5.7.26</b>
+
+- <b>Tomcat 7.0.73</b>
+
+- <b>JDK 1.8</b>
+
+
+
+
+## 缩略图
+
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/78821e35-2b45-455a-a07f-a608d90baaec.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/c836de8a-17d4-460c-91af-7d347096806e.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/0344231a-519d-4e73-be35-69038bd0d16c.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/b5ec019d-6d0b-48b9-b3e2-c1b1ef6e9c79.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/f7759d58-1e56-4a2b-bdf1-809a27f7db7a.png)
+
+
---
+++ src/lookMessage/LookMessageServlet.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lookMessage;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class LookMessageServlet extends HttpServlet {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String userName=request.getParameter("userName");
+        try{
+            Connection con=null;
+            Statement stmt=null;
+            ResultSet rs=null;
+            Class.forName("com.mysql.jdbc.Driver");
+            String url="jdbc:mysql://localhost:3306/graduation_204_person?useUnicode=true&characterEncoding=gbk";
+            con=DriverManager.getConnection(url,"root","123456");
+            stmt=con.createStatement();
+            String sql="select * from user where userName='"+userName+"'";
+            rs=stmt.executeQuery(sql);
+            LookMessageBean mm=new LookMessageBean();
+            while(rs.next()){
+                mm.setName(rs.getString("name"));
+                mm.setSex(rs.getString("sex"));
+                mm.setBirth(rs.getString("birth"));
+                mm.setNation(rs.getString("nation"));
+                mm.setEdu(rs.getString("edu"));
+                mm.setWork(rs.getString("work"));
+                mm.setPhone(rs.getString("phone"));
+                mm.setPlace(rs.getString("place"));
+                mm.setEmail(rs.getString("email"));
+            }
+            HttpSession session=request.getSession();
+            ArrayList wordlist=wordlist=new ArrayList();
+            wordlist.add(mm);
+            session.setAttribute("wordlist", wordlist);
+            rs.close();
+            stmt.close();
+            con.close();
+            response.sendRedirect("http://localhost:8084/PIMS/lookMessage/lookMessage.jsp");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
